6fa7483529c02b52e42719fa2d209e09aec6461c
erikdvoss <erikdvoss@gmail.com>
2015-07-29 16:35:00 -0400

    finished base models

--- bitcoin_trade/trade_engine/models.py
+++ bitcoin_trade/trade_engine/models.py
@@ -1,4 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-class Profile():
+class UserProfile(models.Model):
+    user = models.ForeignKey(User)
+    api_key = models.CharField(max_length=50)
+    secret = models.CharField(max_length=50)
 
+
+class Balance(models.Model):
+    user = models.ForeignKey(User)
+    btc = models.DecimalField(max_digits=12, decimal_places=8)
+    usd = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class Trade(models.Model):
+    user = models.ForeignKey(User)
+    pair = models.CharField(max_length=8, default="btc_usd")
+    type = models.CharField(max_length=4)
+    rate = models.DecimalField(max_digits=5, decimal_places=2)
+    amount = models.DecimalField(max_digits=12, decimal_places=8)
+
+
+class CancelOrder(models.Model):
+    user = models.ForeignKey(User)
+    order_id = models.IntegerField()
