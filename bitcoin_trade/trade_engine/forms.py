3e5414e270e12c43c5d8777ceccceab05afc5b55
erikdvoss <erikdvoss@gmail.com>
2015-07-30 14:38:24 -0400

    finished user profile crud

--- bitcoin_trade/trade_engine/forms.py
+++ bitcoin_trade/trade_engine/forms.py
@@ -1,7 +1,1 @@
-from trade_engine.models import UserProfile
-from django import forms
 
-class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ("user", "api_key", "secret")
